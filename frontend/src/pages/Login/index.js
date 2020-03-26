f31334cb65fd86128c15e5ee16a9412d09dbe652
Marcio Fiorenza <marciofiorenza@gmail.com>
2020-03-25 21:57:10 -0300

    Tela cadastro finalizada, iniciada tela de listagem de casos

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import {FiLogIn} from 'react-icons/fi';
 
 import './styles.css';
@@ -15,7 +16,7 @@ export default function Login() {
                     <h1>Faça seu login</h1>
                     <input type="text" placeholder="Sua ID"/>
                     <button className="button" type="submit">Entrar</button>
-                    <a href="/register"><FiLogIn size={16} color="#E02041" /> Nao tenho cadastro</a>
+                    <Link to="/register" className="back-link"><FiLogIn size={16} color="#E02041" /> Não tenho cadastro</Link>
                 </form>
             </section>
             <img src={heroesImg} alt="Heroes"/>
